9e8907d998db13f182ea6162772e015dcf29eb50
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-16 22:15:45 +0100

    Local Environment Setup for React Development

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import  from "./e";
+
+
+function App() {
+  return (
+    <div>
+      <h1> 
+        <span>😀Emoconopedia</span>
+     </h1>
+     
+    </div>
+  );
+}
+
+export default App;
