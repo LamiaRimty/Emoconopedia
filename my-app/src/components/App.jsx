70813e7b2299437749847b5e8d144a798225d283
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-16 22:40:31 +0100

    Made a emoji dictionary in the App.jsx

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -9,6 +9,47 @@ function App() {
         <span>😀Emoconopedia</span>
      </h1>
      
+     <dl className="dictionary">
+
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Winking Face with Tongue">😜</span>
+        <span>Winking Face with Tongue</span>
+      </dt>
+       
+       <dd>
+       A yellow face sticking out its tongue and making a zany wink, usually shown with its right eye closed and left wide open. Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking.Used in sharing funny jokes between friends. 
+       </dd>
+
+    </div>
+
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Melting Face">🫠</span>
+        <span>Melting Face</span>
+      </dt>
+       
+       <dd>
+       A yellow smiley face melting into a puddle.The eyes and mouth slip down the face, yet still maintain a distorted smile. This quality lends this emoji to sarcasm.Can be used literally to talk about extreme heat. Can also be used metaphorically to talk about embarrassment, shame, a slowly sinking sense of dread, or feeling overwhelmed in some way.
+       </dd>
+
+    </div>
+
+
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Face with Peeking Eye">🫣</span>
+        <span>Face with Peeking Eye</span>
+      </dt>
+       
+       <dd>
+       A yellow face with its hands over its eyes and raised eyebrows. One wide eye is peeking through the fingers.Can be used to express the duality of wanting to look away from something because it's frightening, disgusting, or embarrassing, but not being able to.
+       </dd>
+
+    </div>
+
+     </dl>
+
     </div>
   );
 }
