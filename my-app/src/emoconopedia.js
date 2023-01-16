89155ea6ec78369fb040532077cdffc04d33c2dd
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-16 22:47:11 +0100

    Create emoconopedia.js

---
+++ my-app/src/emoconopedia.js
@@ -0,0 +1,23 @@
+const emoconopedia=[
+
+    {
+        id:1,
+        emoji:"😜",
+        name:"Winking Face with Tongue",
+        meaning:"A yellow face sticking out its tongue and making a zany wink, usually shown with its right eye closed and left wide open. Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking.Used in sharing funny jokes between friends."
+    },
+
+    {
+        id:2,
+        emoji:"🫠",
+        name:"Melting Face",
+        meaning:"A yellow smiley face melting into a puddle.The eyes and mouth slip down the face, yet still maintain a distorted smile. This quality lends this emoji to sarcasm.Can be used literally to talk about extreme heat. Can also be used metaphorically to talk about embarrassment, shame, a slowly sinking sense of dread, or feeling overwhelmed in some way."
+    },
+
+    {
+        id:3,
+        emoji:"🫣",
+        name:"Face with Peeking Eye",
+        meaning:"A yellow face with its hands over its eyes and raised eyebrows. One wide eye is peeking through the fingers.Can be used to express the duality of wanting to look away from something because it's frightening, disgusting, or embarrassing, but not being able to."
+    }
+]
